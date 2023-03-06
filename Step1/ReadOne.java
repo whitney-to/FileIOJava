c46a76928a97bb6159ee12bb00aa9dcc8e4f1c81
Nguyet <nguyet@Zipcoders-MacBook-Pro-103.local>
2023-03-06 11:26:16 -0500

    modified file path to include src file at beginnign in path

--- Step1/ReadOne.java
+++ Step1/ReadOne.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Paths;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
@@ -12,21 +15,20 @@ public class ReadOne
         try {
             // Create the Scanner object using the file
             // as input
-            Scanner fileIn = new Scanner(new File("input.txt"));
-            
+            Scanner fileIn = new Scanner(new File(Paths.get("Step1/input.txt").toUri()));
             /* Loop while the file still has lines.
              * .hasNext() looks to see if a line exists
              * but it doesn't read the line.
              */
-            while (fileIn.hasNext()) 
-            {
+            while (fileIn.hasNext()) {
                 // Reads the entire line
-                String lineIn = fileIn.nextLine(); 
+                String lineIn = fileIn.nextLine();
                 // Output the line
                 System.out.println(lineIn);
             }
         }
         catch (IOException e) {
+            e.printStackTrace();
             System.out.println("File not found");
         }
     }
